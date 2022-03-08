0aa34f489d7030dc8b2d76380f7dfba2c2e82ac2
Alen Budimir <7889389+budi1200@users.noreply.github.com>
2022-03-08 21:46:53 +0100

    Add React router

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
+import {MemoryRouter} from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>
   </React.StrictMode>,
   document.getElementById('root')
 )
